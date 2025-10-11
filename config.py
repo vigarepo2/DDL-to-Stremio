e9db747a9142b5ddc1bb693db4770200e61ffbcd
Viga Bots <133951124+vigarepo2@users.noreply.github.com>
2025-10-11 20:41:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,6 @@ from os import getenv
 from dotenv import load_dotenv
 import hashlib
 
-# Load environment variables from a file named 'config.env'
 load_dotenv("config.env")
 
 class Settings:
@@ -10,16 +9,16 @@ class Settings:
     BASE_URL = getenv("BASE_URL", "http://127.0.0.1:8000").rstrip('/')
     PORT = int(getenv("PORT", "8000"))
 
-    # Database connection string
+    # Database connection
     MONGO_URI = getenv("MONGO_URI", "")
+    DB_NAME = "ddl_stremio_premium"
 
-    # The Movie Database (TMDb) API Key
+    # API Keys
     TMDB_API_KEY = getenv("TMDB_API_KEY", "")
 
-    # Admin Panel credentials
+    # Admin Panel Credentials
     ADMIN_USERNAME = getenv("ADMIN_USERNAME", "admin")
     ADMIN_PASSWORD = getenv("ADMIN_PASSWORD", "admin")
     ADMIN_PASSWORD_HASH = hashlib.sha256(ADMIN_PASSWORD.encode()).hexdigest()
 
-# Create a single instance of the settings to be used across the app
 settings = Settings()
